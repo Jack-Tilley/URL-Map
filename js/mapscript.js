86e30b39443395ca34bba3747e61802ce7fce2cc
Jack-Tilley <tilley.e.jack@gmail.com>
2020-02-16 19:10:42 -0500

    changed some formatting

--- js/mapscript.js
+++ js/mapscript.js
@@ -1,49 +1,142 @@
-var canvas = document.querySelector("canvas"),
-    context = canvas.getContext("2d"),
-    width = canvas.width,
-    height = canvas.height;
+var colors = d3.scaleOrdinal(d3.schemeCategory10);
+
+var svg = d3.select("svg"),
+    width = +svg.attr("width"),
+    height = +svg.attr("height"),
+    node,
+    link;
+
+svg.append('defs').append('marker')
+    .attrs({'id':'arrowhead',
+        'viewBox':'-0 -5 10 10',
+        'refX':13,
+        'refY':0,
+        'orient':'auto',
+        'markerWidth':13,
+        'markerHeight':13,
+        'xoverflow':'visible'})
+    .append('svg:path')
+    .attr('d', 'M 0,-5 L 10 ,0 L 0,5')
+    .attr('fill', '#999')
+    .style('stroke','none');
 
 var simulation = d3.forceSimulation()
-    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    .force("link", d3.forceLink().id(function (d) {return d.id;}).distance(100).strength(1))
     .force("charge", d3.forceManyBody())
-    .force("center", d3.forceCenter());
+    .force("center", d3.forceCenter(width / 2, height / 2));
 
-d3.json("./json_files/nodesandlinks.json", function(error, graph) {
+d3.json("graph.json", function (error, graph) {
     if (error) throw error;
+    update(graph.links, graph.nodes);
+})
+
+function update(links, nodes) {
+    link = svg.selectAll(".link")
+        .data(links)
+        .enter()
+        .append("line")
+        .attr("class", "link")
+        .attr('marker-end','url(#arrowhead)')
+
+    link.append("title")
+        .text(function (d) {return d.type;});
+
+    edgepaths = svg.selectAll(".edgepath")
+        .data(links)
+        .enter()
+        .append('path')
+        .attrs({
+            'class': 'edgepath',
+            'fill-opacity': 0,
+            'stroke-opacity': 0,
+            'id': function (d, i) {return 'edgepath' + i}
+        })
+        .style("pointer-events", "none");
+
+    edgelabels = svg.selectAll(".edgelabel")
+        .data(links)
+        .enter()
+        .append('text')
+        .style("pointer-events", "none")
+        .attrs({
+            'class': 'edgelabel',
+            'id': function (d, i) {return 'edgelabel' + i},
+            'font-size': 10,
+            'fill': '#aaa'
+        });
+
+    edgelabels.append('textPath')
+        .attr('xlink:href', function (d, i) {return '#edgepath' + i})
+        .style("text-anchor", "middle")
+        .style("pointer-events", "none")
+        .attr("startOffset", "50%")
+        .text(function (d) {return d.type});
+
+    node = svg.selectAll(".node")
+        .data(nodes)
+        .enter()
+        .append("g")
+        .attr("class", "node")
+        .call(d3.drag()
+                .on("start", dragstarted)
+                .on("drag", dragged)
+            //.on("end", dragended)
+        );
+
+    node.append("circle")
+        .attr("r", 5)
+        .style("fill", function (d, i) {return colors(i);})
+
+    node.append("title")
+        .text(function (d) {return d.id;});
+
+    node.append("text")
+        .attr("dy", -3)
+        .text(function (d) {return d.name+":"+d.label;});
 
     simulation
-        .nodes(graph.nodes)
+        .nodes(nodes)
         .on("tick", ticked);
 
     simulation.force("link")
-        .links(graph.links);
-
-    function ticked() {
-        context.clearRect(0, 0, width, height);
-        context.save();
-        context.translate(width / 2, height / 2 + 40);
-
-        context.beginPath();
-        graph.links.forEach(drawLink);
-        context.strokeStyle = "#aaa";
-        context.stroke();
-
-        context.beginPath();
-        graph.nodes.forEach(drawNode);
-        context.fill();
-        context.strokeStyle = "#fff";
-        context.stroke();
-
-        context.restore();
-    }
-});
-
-function drawLink(d) {
-    context.moveTo(d.source.x, d.source.y);
-    context.lineTo(d.target.x, d.target.y);
+        .links(links);
 }
 
-function drawNode(d) {
-    context.moveTo(d.x + 3, d.y);
-    context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
+function ticked() {
+    link
+        .attr("x1", function (d) {return d.source.x;})
+        .attr("y1", function (d) {return d.source.y;})
+        .attr("x2", function (d) {return d.target.x;})
+        .attr("y2", function (d) {return d.target.y;});
+
+    node
+        .attr("transform", function (d) {return "translate(" + d.x + ", " + d.y + ")";});
+
+    edgepaths.attr('d', function (d) {
+        return 'M ' + d.source.x + ' ' + d.source.y + ' L ' + d.target.x + ' ' + d.target.y;
+    });
+
+    edgelabels.attr('transform', function (d) {
+        if (d.target.x < d.source.x) {
+            var bbox = this.getBBox();
+
+            rx = bbox.x + bbox.width / 2;
+            ry = bbox.y + bbox.height / 2;
+            return 'rotate(180 ' + rx + ' ' + ry + ')';
+        }
+        else {
+            return 'rotate(0)';
+        }
+    });
+}
+
+function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+    d.fx = d.x;
+    d.fy = d.y;
+}
+
+function dragged(d) {
+    d.fx = d3.event.x;
+    d.fy = d3.event.y;
 }
