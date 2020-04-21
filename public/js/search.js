1ccb25557e05e6c930484d1dcebda63e6dc215a3
Dominic Taraska <tug76525@temple.edu>
2020-04-21 04:13:20 -0400

    Added extended functionality to search

--- public/js/search.js
+++ public/js/search.js
@@ -7,24 +7,38 @@ function searchURL(filepath, target){
             var myObj = JSON.parse(this.responseText);
             var len = myObj["nodes"].length;
             var flag = 0;
+            var flag2 = 0;
             var url;
+            var count = 0;
 
             for(var i = 0; i < len; i++){
                 url = myObj["nodes"][i]["id"];
                 console.log(i);
                 console.log(target.value);
                 console.log(url);
+                if(url.indexOf(target.value) != -1){
+                    count++;
+                    flag2 = 1;
+                }
                 if (url == target.value){
                     flag = 1;
                     break;
                 }
             }
-
+            
             if (flag == 1){
-                document.getElementById("search_result").innerHTML = target.value + " is found in the URL map";
+                document.getElementById("search_result").innerHTML = target.value + " is found in the URL map(exactly)";
+                
             }
+        
             else{
                 document.getElementById("search_result").innerHTML = target.value + " is not found in the URL map";
+            }
+            if (flag2 == 1){
+                document.getElementById("search_result").innerHTML += "; '" + target.value  + "' is found in the URL map " + count + " times.";
+            }
+            else{
+                document.getElementById("search_result").innerHTML += "; '" + target.value + "' is not found in the URL map";
             }
         }
         else{
