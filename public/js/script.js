c097f42e301178b42aea12bc70e789e801572a17
Sarah Noshin <tuf46965@temple.edu>
2020-04-20 23:50:26 -0400

    github repo opens in new tab

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+//hamburgermenu
+
+function onClickMenu(){
+	document.getElementById("menu").classList.toggle("change");
+	document.getElementById("nav").classList.toggle("change");
+	
+	document.getElementById("menu-bg").classList.toggle("change-bg");
+}
+
